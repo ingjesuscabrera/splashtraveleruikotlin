9f11207da33d9e4882fe22d90cf8a1f7f018d51f
cabrerajesus <jesus.cabrera@capital-minds.com>
2024-02-26 10:40:42 -0400

    Generando un spalsh screen en local

---
+++ app/src/main/java/navigation/example/newtravelerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package navigation.example.newtravelerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
